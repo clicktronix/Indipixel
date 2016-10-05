13e838bdc79e6035a8456ae9716590bea1e5ef6c
Vladislav Manakov <s28.morgan@gmail.com>
2016-10-05 14:53:41 +0700

    Создан webpack.config, который делает сборку css и html файлов

--- indipixel/frontend/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require("./css/main.styl");
-require("./css/fonts.styl");
-require("./main.pug");
--- indipixel/webpack.config.js
+++ indipixel/webpack.config.js
@@ -1,13 +1,18 @@
-/**
- * Created by Владислав on 02.10.2016.
- */
 'use strict';
 
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var cssExtractor  = new ExtractTextPlugin(
+    './frontend/css/[name].css');
+var stylExtractor  = new ExtractTextPlugin(
+    './frontend/css/[name].css');
+
 module.exports = {
-    entry: "./frontend/main",
+    entry: [
+        './frontend/css/main.styl',
+        './frontend/css/fonts.styl'
+    ],
     output: {
         path: "./public",
         filename: "[name].js"
@@ -16,9 +21,11 @@ module.exports = {
     module: {
         loaders: [{
             test: /\.pug$/,
-            // loader: ExtractTextPlugin.extract('pug-html-loader')
-            loader: "pug-html-loader"
+            loader: "pug"
         }, {
+        //     test: /\.css$/,
+        //     loader: cssExtractor.extract('style!css')
+        // }, {
             test: /\.styl$/,
             loader: ExtractTextPlugin.extract('css!stylus?resolve url')
         }, {
@@ -28,7 +35,12 @@ module.exports = {
     },
 
     plugins: [
-        new HtmlWebpackPlugin(),
-        new ExtractTextPlugin('[name].css')
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './frontend/index.pug'
+        }),
+        new ExtractTextPlugin('./frontend/css/[name].css', {allChunks: true}),
+        // stylExtractor,
+        // cssExtractor
     ]
 };
